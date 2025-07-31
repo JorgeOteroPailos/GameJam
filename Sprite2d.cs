00dc75797941553ae5d238820713072c0397a251
Jorge Otero Pailos <jorgeoteropailos@gmail.com>
2025-07-31 13:15:10 +0200

    inicializamos

---
+++ Sprite2d.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public partial class Sprite2d : Node2D
+{
+	public float Speed = 200f;
+
+	public override void _Process(double delta)
+	{
+		Vector2 velocity = Vector2.Zero;
+
+		if (Input.IsKeyPressed(Key.W)) velocity.Y -= 1;
+		if (Input.IsKeyPressed(Key.S)) velocity.Y += 1;
+		if (Input.IsKeyPressed(Key.A)) velocity.X -= 1;
+		if (Input.IsKeyPressed(Key.D)) velocity.X += 1;
+
+		Position += velocity.Normalized() * Speed * (float)delta;
+	}
+}
