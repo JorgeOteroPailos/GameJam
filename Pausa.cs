148b64098f8e724de186e3ac22d1d3cdbdaea5c5
Gabriel Novo <gabinv2016@gmail.com>
2025-08-01 21:19:07 +0200

    Musica

--- Pausa.cs
+++ Pausa.cs
@@ -3,12 +3,15 @@ using System;
 
 public partial class Pausa : Node2D{
 	private PauseMenu pauseMenu;
+	private AudioStreamPlayer2D musica;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready(){
 		pauseMenu = GetParent().GetNode<PauseMenu>("PauseMenu");
-		ProcessMode = Node.ProcessModeEnum.Always;	
+		musica = GetParent().GetNode<AudioStreamPlayer2D>("Musica");
 		
+		ProcessMode = Node.ProcessModeEnum.Always;	
+		musica.ProcessMode = Node.ProcessModeEnum.Always;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,8 +19,10 @@ public partial class Pausa : Node2D{
 	
 		if(Input.IsActionJustPressed("Escape")){
 			if (pauseMenu.Visible){
+				musica.VolumeDb=0;
 				pauseMenu.HideMenu();
 			}else{
+				musica.VolumeDb=-6;
 				pauseMenu.ShowMenu();
 			}
 		}
--- PauseMenu.cs
+++ PauseMenu.cs
@@ -3,10 +3,19 @@ using System;
 
 public partial class PauseMenu : CanvasLayer
 {
+	private AudioStreamPlayer2D musica;
+	
 	private Button continuarButton;
 	private Button salirButton;
 	private Button optionsButton;
 	private Button menuButton;
+	
+	private Button muchoSonidoButton;
+	private Button sonidoParcialButton;
+	private Button pocoSonidoButton;
+	private Button mutedButton;
+	private Button ajustesButton;
+	private Button logrosButton;
 	
 	private bool flagOptions=false;
 	
@@ -16,6 +25,8 @@ public partial class PauseMenu : CanvasLayer
 	private VBoxContainer contenedor3;
 
 	public override void _Ready(){
+		musica = GetParent().GetNode<AudioStreamPlayer2D>("Musica");
+		
 		contenedor=GetNode<VBoxContainer>("VBoxContainer");
 		contenedor2=GetNode<VBoxContainer>("VBoxContainer2");
 		contenedor3=GetNode<VBoxContainer>("VBoxContainer3");
@@ -29,16 +40,29 @@ public partial class PauseMenu : CanvasLayer
 		optionsButton = GetNode<Button>("VBoxContainer/Options");
 		menuButton = GetNode<Button>("VBoxContainer/Menu");
 		salirButton = GetNode<Button>("VBoxContainer/Salir");
-
+		
+		muchoSonidoButton = GetNode<Button>("VBoxContainer2/muchoSonido");
+		sonidoParcialButton = GetNode<Button>("VBoxContainer2/sonidoParcial");
+		pocoSonidoButton = GetNode<Button>("VBoxContainer2/pocoSonido");
+		mutedButton = GetNode<Button>("VBoxContainer3/muted");
+		ajustesButton = GetNode<Button>("VBoxContainer3/ajustes");
+		logrosButton = GetNode<Button>("VBoxContainer3/logros");
 
 		continuarButton.Pressed += OnContinuarPressed;
 		optionsButton.Pressed += OnOptionsPressed;
 		menuButton.Pressed += OnMenuPressed;
 		salirButton.Pressed += OnSalirPressed;
 
-
+		muchoSonidoButton.Pressed += OnMuchoVolumenPressed;
+		sonidoParcialButton.Pressed += OnMedioVolumenPressed;
+		pocoSonidoButton.Pressed += OnPocoVolumenPressed;
+		mutedButton.Pressed += OnMutedPressed;
+		ajustesButton.Pressed += OnAjustesPressed;
+		logrosButton.Pressed += OnLogrosPressed;
+		
 		Visible = false; // Empieza oculto
 		ProcessMode = ProcessModeEnum.Always; // Para que reciba input en pausa
+		
 	}
 
 	public void ShowMenu()
@@ -77,10 +101,29 @@ public partial class PauseMenu : CanvasLayer
 	}
 	
 	private void OnMenuPressed(){
-		
+		//De momento nada jaja
 	}
 	
 	private void OnSalirPressed(){
+		GetTree().Quit();
+	}
+	
+	private void OnMuchoVolumenPressed(){
+		musica.VolumeDb=0;
+	}
+	private void OnMedioVolumenPressed(){
+		musica.VolumeDb=-6;
+	}
+	private void OnPocoVolumenPressed(){
+		musica.VolumeDb=-12;
+	}
+	private void OnMutedPressed(){
+		musica.VolumeDb=-80;
+	}
+	private void OnAjustesPressed(){
+		GetTree().Quit();
+	}
+	private void OnLogrosPressed(){
 		GetTree().Quit();
 	}
 }
