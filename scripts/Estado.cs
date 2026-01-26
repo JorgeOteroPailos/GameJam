f57d3f3df1a7d9fc770dad6d277121ca9ed49f8d
jorge <jorgeoteropailos@gmail.com>
2026-01-26 05:12:15 +0100

    añadido el español

--- scripts/Estado.cs
+++ scripts/Estado.cs
@@ -4,7 +4,7 @@ using System;
 public static class Estado{
 	public static int nivel=0;
 	public static int indiceNovela=0;
-	public static string[] idiomasDisponibles={"gal", "en"};
+	public static string[] idiomasDisponibles={"gal", "en", "es"};
 	public static string idioma="gal";
 	public static int[] nEnemigos={12, 16, 20, 24, 13};
 }
